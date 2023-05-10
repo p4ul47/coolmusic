286a9c0edb2bd45be5e2032fce7f0549d75f1da8
p4ul47 <114050463+p4ul47@users.noreply.github.com>
2023-05-10 09:41:44 +0300

    Add files via upload

---
+++ velvienahuina.js
@@ -0,0 +1,5 @@
+const coolbutton = document.querySelector('.js-cool')
+
+coolbutton.addEventListener('click', () => {
+    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley')
+})
